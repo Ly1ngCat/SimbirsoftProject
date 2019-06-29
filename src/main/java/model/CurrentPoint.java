ed6f5a4572bca165967df438c17b5f5dab532811
mindgame73 <mindgame73rus@gmail.com>
2019-06-29 20:12:42 +0400

    added DialogDateTimePicker when clicking, spliting JFrame and add table(just testing now)

--- src/main/java/model/CurrentPoint.java
+++ src/main/java/model/CurrentPoint.java
@@ -1,15 +1,15 @@
 package model;
 
+import java.security.Timestamp;
 import java.time.LocalDateTime;
 
 public class CurrentPoint {
     public double longitude = 0;
     public double latitude = 0;
-<<<<<<< HEAD
+
     public Timestamp currentDate; //поменять название на forecastDate, ибо мы желаемую дату, а не текущую, ищем
-=======
+
     public LocalDateTime currentDate;
->>>>>>> Simba
     public String adressString = "";
 
     public CurrentPoint(double longitude, double latitude,  String adressString, LocalDateTime currentDate) {
