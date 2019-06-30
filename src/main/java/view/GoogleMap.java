857728292f4a1d08b059abe955ecf194d0a84e07
mindgame73 <mindgame73rus@gmail.com>
2019-06-30 20:05:05 +0400

    added date range limits from today

--- src/main/java/view/GoogleMap.java
+++ src/main/java/view/GoogleMap.java
@@ -159,6 +159,7 @@ public class GoogleMap extends MapView {
             setLayout(new FlowLayout());
             JLabel jLabel = new JLabel("Выберите дату и время");
             DateTimePicker dateTimePicker = new DateTimePicker();
+            dateTimePicker.getDatePicker().getSettings().setDateRangeLimits(LocalDate.now(),null);
             JButton jButton = new JButton("Выбрать");
             add(jLabel);
             add(dateTimePicker);
