5f77c34c106810582b2b4d12b12278efed651f21
LyingCat <LyingCat>
2019-07-02 12:05:14 +0300

    Split Googlemap on Visual and Map

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import com.teamdev.jxmaps.LatLng;
 import model.CurrentPoint;
 import net.aksingh.owmjapis.api.APIException;
 import org.json.JSONException;
+import view.GUI.VisuaIinterface;
 import view.GoogleMap;
 import controllers.openweathermapclasses.*;
 
@@ -12,6 +13,6 @@ public class Main {
 
     public static void main(String[] args) throws APIException, JSONException, IOException {
 
-        GoogleMap.Interface(args);
+        VisuaIinterface.Interface(args);
     }
 }
