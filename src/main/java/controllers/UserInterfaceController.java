16175d1ef1dae866bbeabd7cb920c608b78c82db
mindgame73 <mindgame73rus@gmail.com>
2019-07-03 17:59:09 +0400

    added Controller again

--- src/main/java/controllers/UserInterfaceController.java
+++ src/main/java/controllers/UserInterfaceController.java
@@ -87,7 +87,7 @@ public class UserInterfaceController {
         recomendArea.setLineWrap(true);
         recomendArea.setFont(new Font("Arial", Font.PLAIN, 14));
         recomendArea.setBackground(new Color(57, 237, 152));
-        recomendFrame.setTitle("Рекомендации для Ваших путешествий");
+        recomendFrame.setTitle("Рекомендации для ваших путешествий");
         JScrollPane jScrollPane = new JScrollPane(recomendArea);
         jScrollPane.setPreferredSize(new Dimension(600, Toolkit.getDefaultToolkit().getScreenSize().height - 200));
         recomendFrame.add(jScrollPane);
